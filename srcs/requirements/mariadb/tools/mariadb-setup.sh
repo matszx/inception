faac3e23aa7020a9cbbef4efb11e02fbf1ea1778
mcygan <mcygan@disroot.org>
2026-02-06 12:55:50 +0100

    SCRIPTS (idk what I changed)

--- srcs/requirements/mariadb/tools/mariadb-setup.sh
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+chmod -R 755 /home/mcygan/data/db
+chown -R 999:999 /home/mcygan/data/db
+
 mysql_install_db
 mysqld_safe &
-sleep 5
 
-mysql -u root <<EOF
+until mysqladmin ping &> /dev/null;
+do
+	echo "Waiting for database..."
+	sleep 5
+done
+
+mysql -u root -p ${DB_ROOT_PWD} <<EOF
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -11,7 +11,7 @@ do
 	sleep 5
 done
 ./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PWD} --dbhost=${DB_HOST} --allow-root
-./wp-cli.phar core install --url=${URL} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PWD} --admin_email=${EMAIL} --allow-root
+./wp-cli.phar core install --url=${URL} --title=inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --admin_email=${EMAIL} --allow-root
 ./wp-cli.phar user create ${WP_USER} ${WP_USER}@inception.com --role=subscriber --user_pass=${WP_USER_PWD} --allow-root
 
 php-fpm8.2 -F
