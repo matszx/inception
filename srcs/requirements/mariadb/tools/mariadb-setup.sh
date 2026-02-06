731a30569e7cd15b5f5402aa8456be07ed4395f0
mcygan <mcygan@disroot.org>
2026-02-06 13:30:26 +0100

    small change

--- srcs/requirements/mariadb/tools/mariadb-setup.sh
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -12,7 +12,7 @@ do
 	sleep 5
 done
 
-mysql -u root -p ${DB_ROOT_PWD} <<EOF
+mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
