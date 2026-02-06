fff6fd00a5ac24af1c1ec30ea9917e5074ec9c41
mcygan <mcygan@disroot.org>
2026-02-06 03:53:53 +0100

    .env

--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -4,9 +4,9 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 sleep 5
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=mcygan.42.fr --title=inception --admin_user=mcygan --admin_password=1234 --admin_email=mcygan@inception.com --allow-root
-./wp-cli.phar user create guest guest@inception.com --role=subscriber --user_pass=1234 --allow-root
+./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PWD} --dbhost=${DB_HOST} --allow-root
+./wp-cli.phar core install --url=${URL} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PWD} --admin_email=${EMAIL} --allow-root
+./wp-cli.phar user create ${WP_USER} ${WP_USER}@inception.com --role=subscriber --user_pass=${WP_USER_PWD} --allow-root
 
 php-fpm8.2 -F
 
