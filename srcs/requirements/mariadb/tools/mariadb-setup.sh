2f1567cb11f71a055229ed212ca9a1961f5abdf4
matszx <coding@mcygan.anonaddy.me>
2026-02-06 17:19:16 +0100

    cleanup

--- srcs/requirements/mariadb/tools/mariadb-setup.sh
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-#chmod -R 755 /home/mcygan/data/db
-#chown -R 999:999 /home/mcygan/data/db
-
 mysql_install_db
 mysqld_safe &
-
 until mysqladmin ping &> /dev/null;
 do
 	echo "Waiting for database..."
-	sleep 5
+	sleep 2
 done
-
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-
 mysqladmin shutdown
-exec mysqld
 
+exec mysqld
--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -3,16 +3,16 @@
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-chown -R www-data:www-data /var/www/html
+#chown -R www-data:www-data /var/www/html
+
 ./wp-cli.phar core download --allow-root
 until mysql -h ${DB_HOST} -u ${DB_USER} -p ${DB_USER_PWD} ${DB_NAME} &> /dev/null
 do
 	echo "Waiting for database..."
-	sleep 5
+	sleep 2
 done
 ./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PWD} --dbhost=${DB_HOST} --allow-root
 ./wp-cli.phar core install --url=${URL} --title=inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --admin_email=${EMAIL} --allow-root
 ./wp-cli.phar user create ${WP_USER} ${WP_USER}@inception.com --role=subscriber --user_pass=${WP_USER_PWD} --allow-root
 
-php-fpm8.2 -F
-
+exec php-fpm8.2 -F
